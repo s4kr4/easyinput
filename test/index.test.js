32250a045f0c2f4b5d2547e893c256a0492189f2
s4kr4 <s4kr4m4@gmail.com>
2017-05-18 17:39:14 +0900

    Update test

--- test/index.test.js
+++ test/index.test.js
@@ -10,32 +10,35 @@ describe('EasyInputClass', () => {
   })
 
   context('handleKeyDown()', () => {
-    it('Ctrl + A keys return Ctrl+A', () => {
+    it('press "Ctrl + A" return Ctrl+A', () => {
       assert.strictEqual(easyInput.handleKeyDown({
         key: 'A',
+        keyCode: 65,
         ctrlKey: true,
         shiftKey: false,
         altKey: false,
       }), 'Ctrl+A')
     })
 
-    it('Ctrl + Shift + A key returns Ctrl+Shift+A', () => {
+    it('press "Ctrl + Shift + A" returns Ctrl+Shift+A', () => {
       assert.strictEqual(easyInput.handleKeyDown({
         key: 'A',
+        keyCode: 65,
         ctrlKey: true,
         shiftKey: true,
         altKey: false,
       }), 'Ctrl+Shift+A')
     })
 
-    // it('Ctrl + Control key returns Ctrl+', () => {
-    //   assert.strictEqual(easyInput.handleKeyDown({
-    //     key: 'Control',
-    //     ctrlKey: true,
-    //     shiftKey: false,
-    //     altKey: false,
-    //   }), 'Ctrl+')
-    // })
+    it('press "Ctrl" returns Ctrl+', () => {
+      assert.strictEqual(easyInput.handleKeyDown({
+        key: 'Control',
+        keyCode: 17,
+        ctrlKey: true,
+        shiftKey: false,
+        altKey: false,
+      }), 'Ctrl+')
+    })
   })
 })
 
